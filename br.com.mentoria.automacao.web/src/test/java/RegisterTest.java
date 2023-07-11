ec068a1a346e28e04eea82a8af306cd0fe0e3398
cristianoAutomation <tstlinux.c@gmaiil.com>
2023-07-10 23:19:51 -0300

    Criando a primeira classe para abrir o navegador

--- br.com.mentoria.automacao.web/src/test/java/RegisterTest.java
+++ br.com.mentoria.automacao.web/src/test/java/RegisterTest.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class RegisterTest {
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RegisterTest {
+
+    @Test
+    public void register(){
+        //System .setProperty("chromedriver.driver.chrome", "C:\\DRIVER\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.automationtesting.in/Register.html");
+    }
 }

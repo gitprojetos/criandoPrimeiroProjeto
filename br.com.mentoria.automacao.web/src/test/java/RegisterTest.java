14cbc012ae3e436f39d38718f208422b131191aa
cristianoAutomation <tstlinux.c@gmaiil.com>
2023-07-11 01:31:10 -0300

    Aprendendo a escrever nos elementos

--- br.com.mentoria.automacao.web/src/test/java/RegisterTest.java
+++ br.com.mentoria.automacao.web/src/test/java/RegisterTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,5 +10,11 @@ public class RegisterTest {
         //System .setProperty("chromedriver.driver.chrome", "C:\\DRIVER\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://demo.automationtesting.in/Register.html");
+
+        driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Etech");
+        driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("Mentoria");
+        driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys("Teste Endereço");
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("tstlinux.c@gmail.com");
+        driver.findElement(By.xpath("//input[@type='tel']")).sendKeys("8888888888");
     }
 }

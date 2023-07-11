e2f58cd7fc5a19ac3a0c0b57fbea594d4a25d88e
cristianoAutomation <tstlinux.c@gmaiil.com>
2023-07-11 16:05:42 -0300

    Utilizando a biblioteca FAKE

--- br.com.mentoria.automacao.web/src/test/java/br/com/mentoria/automacao/web/RegisterTest.java
+++ br.com.mentoria.automacao.web/src/test/java/br/com/mentoria/automacao/web/RegisterTest.java
@@ -1,23 +1,31 @@
 package br.com.mentoria.automacao.web;
 
 import br.com.mentoria.automacao.configuracao.BaseTest;
+import com.github.javafaker.Faker;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class RegisterTest extends BaseTest {
 
+    Faker faker = new Faker(new Locale("pt-BR"));
+
     @Test
     public void register(){
 
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String address = faker.address().fullAddress();
+
         driver.get("https://demo.automationtesting.in/Register.html");
-        driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Etech");
-        driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("Mentoria");
-        driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys("Teste Endereço");
+        driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys(firstName);
+        driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys(lastName);
+        driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys(address);
         driver.findElement(By.xpath("//input[@type='email']")).sendKeys("tstlinux.c@gmail.com");
         driver.findElement(By.xpath("//input[@type='tel']")).sendKeys("8888888888");
         driver.findElement(By.xpath("//input[@value='Male']")).click();

8b0f48c00378fc961fabf7ad9d7aa42483065eb2
cristianoAutomation <tstlinux.c@gmaiil.com>
2023-07-11 23:34:20 -0300

    Ajustes na classe de teste iframe

--- br.com.mentoria.automacao.web/src/test/java/br/com/mentoria/automacao/web/Switchto.java
+++ br.com.mentoria.automacao.web/src/test/java/br/com/mentoria/automacao/web/Switchto.java
@@ -1,35 +1,42 @@
 package br.com.mentoria.automacao.web;
 
 import commons.DSL;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class Switchto extends DSL {
+
+    @Before
+    public void beforeTest() {
+        acessarSite("https://demo.automationtesting.in/Register.html");
+    }
+
     @Test
     public void actionBySwitchTo() {
-        acessarSite("https://demo.automationtesting.in/Register.html");
         actionByXpath("//a[@class='dropdown-toggle' and text()='SwitchTo']");
         clickByXpath("//a[@style='color: white' and text()='Frames']");
-        fecharBrowser();
     }
 
     @Test
     public void iframeTest() {
-        acessarSite("https://demo.automationtesting.in/Register.html");
         actionByXpath("//a[@class='dropdown-toggle' and text()='SwitchTo']");
         clickByXpath("//a[@style='color: white' and text()='Frames']");
-        clickByXpath("//a[@style='color: white' and text()='Frames']");
-        fecharBrowser();
+//      clickByXpath("//a[@style='color: white' and text()='Frames']");
     }
 
     @Test
     public void multipleIframe() {
-        acessarSite("https://demo.automationtesting.in/Register.html");
         actionByXpath("//a[@class='dropdown-toggle' and text()='SwitchTo']");
         clickByXpath("//a[@style='color: white' and text()='Frames']");
-        clickByXpath("//a[@style='color: white' and text()='Frames']");
-        clickByXpath("//a[@class='analystic' and text()='Iframe with in an Iframe']");
-        iframeByXpath("iframe[@src='MultipleFrames.html']");
-        fecharBrowser();
+//      clickByXpath("//a[@style='color: white' and text()='Frames']");
+//      clickByXpath("//a[@class='analystic' and text()='Iframe with in an Iframe']");
+//      iframeByXpath("iframe[@src='MultipleFrames.html']");
 
+    }
+
+    @After
+    public void afterTest() {
+        fecharBrowser();
     }
 }
